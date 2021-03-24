b4a65e76199205a8ac5e416c3dffae2fe43598aa
Vignesh Bharathi <vigneshbharathi21@gmail.com>
2021-03-24 18:28:28 +0530

    added new task tracker project developed with vue.js

---
+++ projects/vue/task-tracker/src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button text="Add Task" color="green" />
+        
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: {
+            type: String,
+            default: 'asdlkjfh',
+            }
+    },
+    components: { 
+      Button
+    },
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
---
+++ projects/vue/task-tracker/src/components/Tasks.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-bind:for="task in tasks">
+        <h3>{{ task.text }}</h3>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    }
+}
+</script>
